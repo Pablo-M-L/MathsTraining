8a56fa2158d08e3e620f33ca4338efaabb97ad0b
pablo M L <crapula8@hotmail.com>
2019-04-21 23:37:44 +0200

    Initial Commit

---
+++ MathsTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MathsTraining
+//
+//  Created by admin on 21/04/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
